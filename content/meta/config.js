2589537cedd31c2cbd2219c88b0585b0197bc9b0
Zahed Shareef <realzahed@gmail.com>
2018-09-12 23:49:26 +0300

    fix social icons

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,33 @@
-const colors = require("../../src/styles/colors");
+const colors = require('../../src/styles/colors');
 
 module.exports = {
-  siteTitle: "Zahed Shareef", // <title>
-  shortSiteTitle: "Zahed", // <title> ending for posts and pages
-  siteDescription: "Zahed Shareef - Software Engineer",
-  siteUrl: "https://zahedshareef.com/blog",
-  pathPrefix: "",
-  siteImage: "preview.jpg",
-  siteLanguage: "en",
-  // author
-  authorName: "zahed shareef",
-  authorTwitterAccount: "realzahed",
-  // info
-  infoTitle: "zahed shareef",
-  infoTitleNote: "software engineer",
-  // manifest.json
-  manifestName: "Zahed Shareef",
-  manifestShortName: "Zahed Blog", // max 12 characters
-  manifestStartUrl: "/projects",
-  manifestBackgroundColor: colors.background,
-  manifestThemeColor: colors.background,
-  manifestDisplay: "standalone",
-  // contact
-  contactEmail: "zahedkw@gmail.com",
-  // social
-  authorSocialLinks: [
-    { name: "github", url: "https://github.com/zahedshareef" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/zahedshareef/" },
-    { name: "twitter", url: "https://twitter.com/realzahed" }
-  ]
+	siteTitle: 'Zahed Shareef', // <title>
+	shortSiteTitle: 'Zahed', // <title> ending for posts and pages
+	siteDescription: 'Zahed Shareef - Software Engineer',
+	siteUrl: 'https://zahedshareef.com/blog',
+	pathPrefix: '',
+	siteImage: 'preview.jpg',
+	siteLanguage: 'en',
+	// author
+	authorName: 'zahed shareef',
+	authorTwitterAccount: 'realzahed',
+	// info
+	infoTitle: 'zahed shareef',
+	infoTitleNote: 'software engineer',
+	// manifest.json
+	manifestName: 'Zahed Shareef',
+	manifestShortName: 'Zahed Blog', // max 12 characters
+	manifestStartUrl: '/projects',
+	manifestBackgroundColor: colors.background,
+	manifestThemeColor: colors.background,
+	manifestDisplay: 'standalone',
+	// contact
+	contactEmail: 'zahedkw@gmail.com',
+	// social
+	authorSocialLinks: [
+		{ name: 'linkedin', url: 'https://www.linkedin.com/in/zahedshareef/' },
+		{ name: 'github', url: 'https://github.com/zahedshareef' },
+		{ name: 'twitter', url: 'https://twitter.com/realzahed' },
+		{ name: 'stackoverflow', url: 'https://stackoverflow.com/users/3918206/zahed' }
+	]
 };
