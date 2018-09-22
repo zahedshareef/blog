9bff5809bce620e6bc6c84b2f296ce1690b34de0
Zahed Shareef <realzahed@gmail.com>
2018-09-22 20:47:13 +0300

    fixed issue in layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -21,157 +21,156 @@ import LayoutWrapper from '../components/LayoutWrapper/';
 import { isWideScreen, timeoutThrottlerHandler } from '../utils/helpers';
 
 const InfoBox = asyncComponent(
-	() =>
-		import('../components/InfoBox/')
-			.then((module) => {
-				return module;
-			})
-			.catch((error) => {}),
-	<Loading overrides={{ width: `${theme.info.sizes.width}px`, height: '100vh', right: 'auto' }} afterRight={true} />
+    () =>
+        import('../components/InfoBox/')
+            .then((module) => {
+                return module;
+            })
+            .catch((error) => {}),
+    <Loading overrides={{ width: `${theme.info.sizes.width}px`, height: '100vh', right: 'auto' }} afterRight={true} />
 );
 
 class Layout extends React.Component {
-	timeouts = {};
-	categories = [];
+    timeouts = {};
+    categories = [];
 
-	componentDidMount() {
-		this.props.setIsWideScreen(isWideScreen());
-		if (typeof window !== 'undefined') {
-			window.addEventListener('resize', this.resizeThrottler, false);
-		}
-	}
+    componentDidMount() {
+        this.props.setIsWideScreen(isWideScreen());
+        if (typeof window !== 'undefined') {
+            window.addEventListener('resize', this.resizeThrottler, false);
+        }
+    }
 
-	componentWillMount() {
-		if (typeof localStorage !== 'undefined') {
-			const inLocal = +localStorage.getItem('font-size-increase');
+    componentWillMount() {
+        if (typeof localStorage !== 'undefined') {
+            const inLocal = +localStorage.getItem('font-size-increase');
 
-			const inStore = this.props.fontSizeIncrease;
+            const inStore = this.props.fontSizeIncrease;
 
-			if (inLocal && inLocal !== inStore && inLocal >= 1 && inLocal <= 1.5) {
-				this.props.setFontSizeIncrease(inLocal);
-			}
-		}
+            if (inLocal && inLocal !== inStore && inLocal >= 1 && inLocal <= 1.5) {
+                this.props.setFontSizeIncrease(inLocal);
+            }
+        }
 
-		this.getCategories();
-	}
+        this.getCategories();
+    }
 
-	getCategories = () => {
-		this.categories = this.props.data.posts.edges.reduce((list, edge, i) => {
-			const category = edge.node.frontmatter.category;
-			if (category && !~list.indexOf(category)) {
-				return list.concat(edge.node.frontmatter.category);
-			} else {
-				return list;
-			}
-		}, []);
-	};
+    getCategories = () => {
+        this.categories = this.props.data.posts.edges.reduce((list, edge, i) => {
+            const category = edge.node.frontmatter.category;
+            if (category && !~list.indexOf(category)) {
+                return list.concat(edge.node.frontmatter.category);
+            } else {
+                return list;
+            }
+        }, []);
+    };
 
-	resizeThrottler = () => {
-		return timeoutThrottlerHandler(this.timeouts, 'resize', 500, this.resizeHandler);
-	};
+    resizeThrottler = () => {
+        return timeoutThrottlerHandler(this.timeouts, 'resize', 500, this.resizeHandler);
+    };
 
-	resizeHandler = () => {
-		this.props.setIsWideScreen(isWideScreen());
-	};
+    resizeHandler = () => {
+        this.props.setIsWideScreen(isWideScreen());
+    };
 
-	render() {
-		const { children, data } = this.props;
+    render() {
+        const { children, data } = this.props;
 
-		// TODO: dynamic management of tabindexes for keybord navigation
-		return (
-			<LayoutWrapper>
-				{children()}
-				<Navigator posts={data.posts.edges} />
-				<ActionsBar categories={this.categories} />
-				<InfoBar pages={data.pages.edges} parts={data.parts.edges} />
-				{this.props.isWideScreen && <InfoBox pages={data.pages.edges} parts={data.parts.edges} />}
-			</LayoutWrapper>
-		);
-	}
+        // TODO: dynamic management of tabindexes for keybord navigation
+        return (
+            <LayoutWrapper>
+                {children()}
+                <Navigator posts={data.posts.edges} />
+                <ActionsBar categories={this.categories} />
+                <InfoBar pages={data.pages.edges} parts={data.parts.edges} />
+                {this.props.isWideScreen && <InfoBox pages={data.pages.edges} parts={data.parts.edges} />}
+            </LayoutWrapper>
+        );
+    }
 }
 
 Layout.propTypes = {
-	data: PropTypes.object.isRequired,
-	children: PropTypes.func.isRequired,
-	setIsWideScreen: PropTypes.func.isRequired,
-	isWideScreen: PropTypes.bool.isRequired,
-	fontSizeIncrease: PropTypes.number.isRequired,
-	setFontSizeIncrease: PropTypes.func.isRequired
+    data: PropTypes.object.isRequired,
+    children: PropTypes.func.isRequired,
+    setIsWideScreen: PropTypes.func.isRequired,
+    isWideScreen: PropTypes.bool.isRequired,
+    fontSizeIncrease: PropTypes.number.isRequired,
+    setFontSizeIncrease: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state, ownProps) => {
-	return {
-		pages: state.pages,
-		isWideScreen: state.isWideScreen,
-		fontSizeIncrease: state.fontSizeIncrease
-	};
+    return {
+        pages: state.pages,
+        isWideScreen: state.isWideScreen,
+        fontSizeIncrease: state.fontSizeIncrease
+    };
 };
 
 const mapDispatchToProps = {
-	setIsWideScreen,
-	setFontSizeIncrease
+    setIsWideScreen,
+    setFontSizeIncrease
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRoot(injectSheet(globals)(Layout)));
 
 //eslint-disable-next-line no-undef
 export const guery = graphql`
-	query LayoutQuery {
-		posts: allMarkdownRemark(
-			filter: { id: { regex: "//posts//" } }
-			sort: { fields: [fields___prefix], order: DESC }
-		) {
-			edges {
-				node {
-					excerpt
-					fields {
-						slug
-						prefix
-					}
-					frontmatter {
-						title
-						subTitle
-						status
-						category
-						cover {
-							children {
-								... on ImageSharp {
-									resolutions(width: 90, height: 90) {
-										...GatsbyImageSharpResolutions_withWebp_noBase64
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		pages: allMarkdownRemark(
-			filter: { id: { regex: "//pages//" }, fields: { prefix: { regex: "/^\\d+$/" } } }
-			sort: { fields: [fields___prefix], order: ASC }
-		) {
-			edges {
-				node {
-					fields {
-						slug
-						prefix
-					}
-					frontmatter {
-						title
-						menuTitle
-					}
-				}
-			}
-		}
-		parts: allMarkdownRemark(filter: { id: { regex: "//parts//" } }) {
-			edges {
-				node {
-					html
-					frontmatter {
-						title
-					}
-				}
-			}
-		}
-	}
+    query LayoutQuery {
+        posts: allMarkdownRemark(
+            filter: { id: { regex: "//posts//" } }
+            sort: { fields: [fields___prefix], order: DESC }
+        ) {
+            edges {
+                node {
+                    excerpt
+                    fields {
+                        slug
+                        prefix
+                    }
+                    frontmatter {
+                        title
+                        subTitle
+                        category
+                        cover {
+                            children {
+                                ... on ImageSharp {
+                                    resolutions(width: 90, height: 90) {
+                                        ...GatsbyImageSharpResolutions_withWebp_noBase64
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        pages: allMarkdownRemark(
+            filter: { id: { regex: "//pages//" }, fields: { prefix: { regex: "/^\\d+$/" } } }
+            sort: { fields: [fields___prefix], order: ASC }
+        ) {
+            edges {
+                node {
+                    fields {
+                        slug
+                        prefix
+                    }
+                    frontmatter {
+                        title
+                        menuTitle
+                    }
+                }
+            }
+        }
+        parts: allMarkdownRemark(filter: { id: { regex: "//parts//" } }) {
+            edges {
+                node {
+                    html
+                    frontmatter {
+                        title
+                    }
+                }
+            }
+        }
+    }
 `;
