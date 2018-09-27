74635f5f37fe37847e9f18aa27d10b9d3a1a6ed2
Zahed Shareef <realzahed@gmail.com>
2018-09-28 00:26:42 +0300

    Add Google Analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,41 +10,41 @@ import createStore from './src/state/store';
 import theme from './src/styles/theme';
 
 exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
-	const pageContext = getPageContext();
-	const store = createStore();
+    const pageContext = getPageContext();
+    const store = createStore();
 
-	replaceBodyHTMLString(
-		renderToString(
-			<Provider store={store}>
-				<JssProvider registry={pageContext.sheetsRegistry} generateClassName={pageContext.generateClassName}>
-					{React.cloneElement(bodyComponent, {
-						pageContext
-					})}
-				</JssProvider>
-			</Provider>
-		)
-	);
+    replaceBodyHTMLString(
+        renderToString(
+            <Provider store={store}>
+                <JssProvider registry={pageContext.sheetsRegistry} generateClassName={pageContext.generateClassName}>
+                    {React.cloneElement(bodyComponent, {
+                        pageContext
+                    })}
+                </JssProvider>
+            </Provider>
+        )
+    );
 
-	setHeadComponents([
-		<style
-			type="text/css"
-			id="server-side-jss"
-			key="server-side-jss"
-			dangerouslySetInnerHTML={{ __html: pageContext.sheetsRegistry.toString() }}
-		/>
-	]);
+    setHeadComponents([
+        <style
+            type="text/css"
+            id="server-side-jss"
+            key="server-side-jss"
+            dangerouslySetInnerHTML={{ __html: pageContext.sheetsRegistry.toString() }}
+        />
+    ]);
 };
 
 exports.onRenderBody = ({ setHeadComponents }) => {
-	return setHeadComponents([]);
+    return setHeadComponents([]);
 };
 
 exports.onRenderBody = ({ setPostBodyComponents }) => {
-	return setPostBodyComponents([
-		<script
-			key={`webfontsloader-setup`}
-			dangerouslySetInnerHTML={{
-				__html: `
+    return setPostBodyComponents([
+        <script
+            key={`webfontsloader-setup`}
+            dangerouslySetInnerHTML={{
+                __html: `
         WebFontConfig = {
           google: {
             families: ["${theme.base.fonts.styledFamily}:${theme.base.fonts.styledFonts}"]
@@ -56,8 +56,16 @@ exports.onRenderBody = ({ setPostBodyComponents }) => {
             wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
             wf.async = true;
             s.parentNode.insertBefore(wf, s);
-        })(document);`
-			}}
-		/>
-	]);
+		})(document);
+		
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject'] = r; i[r]=i[r]||function(){
+				(i[r].q = i[r].q || []).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-55915436-1', 'auto');
+			ga('require', 'GTM-5RH6MX9');
+			ga('send', 'pageview');`
+            }}
+        />
+    ]);
 };
